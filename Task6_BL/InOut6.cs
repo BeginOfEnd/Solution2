5f8a6c148e89419ac02a10ee0d332ba25ffa04df
BeginOfEnd <39407361+BeginOfEnd@users.noreply.github.com>
2018-05-18 22:24:56 +0300

    Add files via upload

---
+++ Task6_BL/InOut6.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task6_BL
+{
+    public class InOut6
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        
+
+        public Queue<double> ReadFromFile(string Filename, Queue<double> numbers)
+        {
+            Text = "";
+            numbers.Clear();
+            StreamReader sr = new StreamReader(Filename);
+            string reader = sr.ReadToEnd();
+            Text += reader;
+            sr.Close();
+            string text = "";
+            int count = 0;
+            List<double> list = new List<double>();
+            for (int i = 0; i < Text.Length; i++)
+            {
+                if (Text[i] == ' ')
+                {
+                    for (int j = count; j < i; j++)
+                    {
+                        text += Text[j];
+                    }
+                    list.Add(Convert.ToDouble(text));
+                    text = "";
+                    ++i;
+                    count = i;
+                }
+            }
+            for (int i = 0; i < list.Count; i++)
+                numbers.Enqueue(list[i]);
+            return numbers;
+        }
+    }
+}
---
+++ Task6_BL/MaxValues.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_BL
+{
+    public class MaxValues
+    {
+        public Queue<double> numbers { get; set; }
+
+        public MaxValues()
+        {
+            Queue<double> QUEUE = new Queue<double>();
+            numbers = QUEUE;
+        }
+
+        public double MaxValue(Queue<double> numbers)
+        {            
+            List<double> list = numbers.ToList();
+            double max = list[0];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] > max)
+                    max = list[i];
+            }
+            return max;
+        }
+        public uint FindAllMaxValues(Queue<double> numbers1)
+        {
+            double max = MaxValue(numbers1);
+            uint count = 0;
+            List<double> list = numbers1.ToList();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] == max)
+                    ++count;
+            }
+            return count;
+        }
+    }
+}
