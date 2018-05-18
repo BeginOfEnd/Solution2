8dd4319a7f0a02d4ddbe41248136ba68f96321e5
Antonjax <Antonjax@ANTONJAX->
2018-05-18 21:54:03 +0300

    Добавьте файлы проекта.

---
+++ Task5_Form/Form5.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Task5_Form
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.StudentsBox = new System.Windows.Forms.TextBox();
+            this.SurnameBox = new System.Windows.Forms.TextBox();
+            this.FileButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputOpenFileDialog
+            // 
+            this.inputOpenFileDialog.DefaultExt = "txt";
+            this.inputOpenFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            // 
+            // StudentsBox
+            // 
+            this.StudentsBox.Location = new System.Drawing.Point(26, 58);
+            this.StudentsBox.Multiline = true;
+            this.StudentsBox.Name = "StudentsBox";
+            this.StudentsBox.Size = new System.Drawing.Size(225, 391);
+            this.StudentsBox.TabIndex = 0;
+            // 
+            // SurnameBox
+            // 
+            this.SurnameBox.Location = new System.Drawing.Point(360, 77);
+            this.SurnameBox.Name = "SurnameBox";
+            this.SurnameBox.Size = new System.Drawing.Size(126, 20);
+            this.SurnameBox.TabIndex = 1;
+            // 
+            // FileButton
+            // 
+            this.FileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FileButton.Location = new System.Drawing.Point(26, 480);
+            this.FileButton.Name = "FileButton";
+            this.FileButton.Size = new System.Drawing.Size(225, 52);
+            this.FileButton.TabIndex = 2;
+            this.FileButton.Text = "Считать из файла";
+            this.FileButton.UseVisualStyleBackColor = true;
+            this.FileButton.Click += new System.EventHandler(this.FileButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.AddButton.Location = new System.Drawing.Point(360, 133);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(126, 31);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(682, 563);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.FileButton);
+            this.Controls.Add(this.SurnameBox);
+            this.Controls.Add(this.StudentsBox);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.OpenFileDialog inputOpenFileDialog;
+        private System.Windows.Forms.TextBox StudentsBox;
+        private System.Windows.Forms.TextBox SurnameBox;
+        private System.Windows.Forms.Button FileButton;
+        private System.Windows.Forms.Button AddButton;
+    }
+}
+
---
+++ Task5_Form/Form5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task5_BL;
+using System.IO;
+
+namespace Task5_Form
+{
+    public partial class Form5 : Form
+    {
+        InOut5  io = new InOut5();
+        WorkWithStudents STUDENTS = new WorkWithStudents();
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void FileButton_Click(object sender, EventArgs e)
+        {
+            if (inputOpenFileDialog.ShowDialog() == DialogResult.OK)
+                try
+                {
+                    inputOpenFileDialog.InitialDirectory = Path.GetDirectoryName(inputOpenFileDialog.FileName);
+                    STUDENTS.students = io.ReadFromFile(inputOpenFileDialog.FileName, STUDENTS.students);
+                }
+                catch (Exception except)
+                {
+                    FormsUtils.ErrorMessageBox(except);
+                }
+            StudentsBox.Text = io.Text;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            string student = SurnameBox.Text;
+            io.Text = "";
+            STUDENTS.students.Enqueue(student);
+            STUDENTS.students.Sort(STUDENTS.students);
+            io.Text = STUDENTS.students.Print(STUDENTS.students);
+            StudentsBox.Text = io.Text;
+        }
+    }
+}
